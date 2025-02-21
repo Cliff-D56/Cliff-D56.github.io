412ab9e974ff103ba2ef79fd83ec8ea05b55779e
Cliff-D56 <cliffordgardner9@gmail.com>
2025-02-20 23:55:43 -0600

    tweaking character skins

--- Hidden_In_Black_Ice/expanding_cards.js
+++ Hidden_In_Black_Ice/expanding_cards.js
@@ -1,3 +1,4 @@
+
 const panels = document.querySelectorAll(".panel")
 panels.forEach((panel)=>{
     panel.addEventListener("click",()=>{
@@ -5,4 +6,25 @@ panels.forEach((panel)=>{
             panel.classList.remove("active")
         })
         panel.classList.add("active")})
+})
+const images = [`edward1.png`,`edward2.png`]
+const edward = document.querySelector("#edward")
+const edwardPics = getPics("edward")
+function getPics(name){
+    let charArray = [];
+    for(let i = 0; i<images.length; i++){
+        if(images[i].includes(name)){
+        console.log(images[i])
+        charArray.push(`./${name}${i+1}.png`);
+    }
+}
+    return charArray;
+}
+// @ts-ignore
+edward.addEventListener("click",()=>{
+    console.log(edward.style.backgroundImage)
+    edward.style.backgroundImage ===`url(${edwardPics[0]}`? 
+    edward.style.backgroundImage = `url(${edwardPics[1]}`: 
+    edward.style.backgroundImage = `url(${edwardPics[0]}`
+    console.log(edward.style.backgroundImage)
 })
